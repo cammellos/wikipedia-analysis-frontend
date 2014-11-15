c98a7c34def5539e19db623cee7a27b141907f99
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2014-11-15 12:45:34 +0000

    celery + state machine

--- edit_history/models.py
+++ edit_history/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django_fsm import FSMField, transition
+from celery import task
+import string
+
+
+import urllib.request
+
 
 # Create your models here.
 
@@ -12,19 +18,26 @@ class Url(models.Model):
     PROCESSING = 2
     COMPLETE = 3
 
+
   STATE_CHOICES = (
-    (STATE.NEW,"New","New"),
-    (STATE.DOWNLOADING,"Downloading","Downloading"),
-    (STATE.PROCESSING, "Processing","Processing"),
-    (STATE.COMPLETE, "Complete","Complete"),
-    (STATE.ERROR,"Failed","Failed")
+    (STATE.NEW,'New','Url'),
+    (STATE.DOWNLOADING,'Downloading','Url'),
+    (STATE.PROCESSING, 'Processing','Url'),
+    (STATE.COMPLETE, 'Complete','Url'),
+    (STATE.ERROR,'Failed','Url')
   )
+
+  BASE_URL = string.Template("http://en.wikipedia.org/w/index.php?title=Special:Export&pages=$name%0ATalk:$name&offset=1&action=submit")
 
   title = models.CharField(max_length=100, unique=True)
   state = FSMField(default=STATE.NEW,state_choices=STATE_CHOICES)
 
+
+  @task()
   @transition(field=state, source=STATE.NEW, target=STATE.DOWNLOADING)
-  def schedule_download(self):
-      """ hta"""
+  def download_page_history(self): 
+    urllib.request.urlretrieve(self.BASE_URL.substitute(name=self.title), filename=string.Template("/tmp/$name.xml").substitute(name=self.title), data=b'none=none')
+      
+
 
   
